01b35af15c37aefed1aa3cd51ff71fd5730ede20
ioana <ioana@192.168.8.160>
2017-07-07 10:17:26 +0300

    Signed-off-by: ioana <ioana@192.168.8.160>

---
+++ kepres2Web/src/main/resources/ro/kepler/kepres2/web/ioana/BradController.java
@@ -0,0 +1,49 @@
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+
+public class BradController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public BradController() {
+        super();
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        
+        
+        PrintWriter out=response.getWriter();  
+          
+        int nrRamuri=Integer.parseInt(request.getParameter("nrRamuri"));  
+         
+          
+        Brad brad = new Brad(nrRamuri);
+        char fg = '*';
+        char bg = ' ';
+        for (int i = 0; i < brad.linii + 2; i++) {
+			for (int j = 0; j < brad.coloane; j++) {
+				if ( brad.matrice[i][j] == 1 ) {
+					out.print(fg);
+				}
+				else {
+					out.print(bg);
+				}
+			}
+			out.println();
+		}
+              
+    }  
+  
+    @Override  
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
+            throws ServletException, IOException {  
+        doPost(req, resp);  
+    }  
+}
