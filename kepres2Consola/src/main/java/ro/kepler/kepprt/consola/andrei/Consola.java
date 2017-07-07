42891853f00aded7569cb4e7570bf380eb345cfd
Dan Maxim <dmaxim@kepler.ro>
2017-07-07 15:14:14 +0300

    Signed-off-by: Dan Maxim <dmaxim@kepler.ro>

--- kepres2Consola/src/main/java/ro/kepler/kepprt/consola/andrei/Consola.java
+++ kepres2Consola/src/main/java/ro/kepler/kepprt/consola/andrei/Consola.java
@@ -3,57 +3,53 @@ package ro.kepler.kepprt.consola.andrei;
 import java.util.Scanner;
 
 public class Consola {
-	private char fg = '*';
+	private char fg = '^';
 	private char bg = ' ';
-	
-	public int citire()
-	{
+
+	public int citire() {
 		System.out.print("Cate ramuri doresti sa aiba bradul? ");
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		
-		if(x <= 0)
-		{
+
+		if (x <= 0) {
 			System.out.print("ERROARE");
 			sc.close();
 			return 0;
-		}
-		else {
+		} else {
 			sc.close();
-		    return x;
+			return x;
 		}
-		     
+
 	}
-	
+
 	public void afisare(Brad brad) {
 		int ramuri = brad.getNrRamuri();
 		int[][] matrice = brad.getMatriceBrad();
-		
+
 		for (int i = 0; i < ((ramuri + 1) * (ramuri + 2)) / 2 + 1; i++) {
 
 			for (int j = 0; j < 2 * ramuri + 1; j++) {
-				if (matrice[i][j] == 0) { 
-					System.out.print(bg); 
+				if (matrice[i][j] == 0) {
+					System.out.print(bg);
 				} else {
-					System.out.print(fg); 
+					System.out.print(fg);
 				}
-				//System.out.print(a[i][j] + " ");
+				// System.out.print(a[i][j] + " ");
 			}
 			System.out.println();
-			
 
 		}
-		
+
 		afisareRamuri(ramuri);
 
 	}
-	
+
 	/*
-	 * Private methods 
+	 * Private methods
 	 */
-	
+
 	private void afisareRamuri(int nrRamuri) {
-		System.out.println("\nNumarul de ramuri ale bradului este: " +nrRamuri);
+		System.out.println("\nNumarul de ramuri ale bradului este: " + nrRamuri);
 	}
 
 }
