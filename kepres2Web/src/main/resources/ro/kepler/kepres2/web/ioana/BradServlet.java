9dea6e08b5d63fc4e583912ef3b1f23da04a8dd9
ioana <ioana@192.168.8.160>
2017-07-07 11:26:21 +0300

    Signed-off-by: ioana <ioana@192.168.8.160>

---
+++ kepres2Web/src/main/resources/ro/kepler/kepres2/web/ioana/BradServlet.java
@@ -0,0 +1,58 @@
+package ro.kepler.kepprt;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+
+public class BradServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	
+    public BradServlet() {
+        super();
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        
+        
+        PrintWriter out=response.getWriter(); 
+        out.println("<html>");
+		out.println("<style>");
+		out.println("body {");
+		out.println("font-family: monospace;");
+		out.println("}");
+		out.println("</style");
+		out.println("<body>");
+          
+        int nrRamuri=Integer.parseInt(request.getParameter("nrRamuri"));  
+         
+          
+        Brad brad = new Brad(nrRamuri);
+        char fg = '*';
+        char bg = ' ';
+        for (int i = 0; i < brad.linii + 2; i++) {
+			for (int j = 0; j < brad.coloane; j++) {
+				if ( brad.matrice[i][j] == 1 ) {
+					out.print(fg);
+				}
+				else {
+					out.print(bg);
+				}
+			}
+			out.println();
+		}
+              
+    }  
+  
+    @Override  
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
+            throws ServletException, IOException {  
+        doPost(req, resp);  
+    }  
+}
