52f73ef2b1f6e86c74e6fb9f07d2c1c959582817
Dan Maxim <dmaxim@kepler.ro>
2017-07-07 14:56:27 +0300

    Signed-off-by: Dan Maxim <dmaxim@kepler.ro>

--- kepres2Consola/src/main/java/ro/kepler/kepres2/consola/alexenache/Main.java
+++ kepres2Consola/src/main/java/ro/kepler/kepres2/consola/alexenache/Main.java
@@ -1,5 +1,7 @@
 package ro.kepler.kepres2.consola.alexenache;
+
 import ro.kepler.kepprt.kepres2.app.alexenache.*;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
