ff227b44f1f170329d9668ff0594e249c3dbc2f6
Toma Alex <intern@intern12>
2017-07-11 16:04:00 +0300

    Signed-off-by: Toma Alex <intern@intern12>

--- kepres2Web/src/main/java/ro/kepler/kepres/web/controller/ControllerAtasament.java
+++ kepres2Web/src/main/java/ro/kepler/kepres/web/controller/ControllerAtasament.java
@@ -3,15 +3,11 @@ package ro.kepler.kepres.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import ro.kepler.kepres.app.dao.DaoAtasament;
 import ro.kepler.kepres.common.dataRecords.Atasament;
 
