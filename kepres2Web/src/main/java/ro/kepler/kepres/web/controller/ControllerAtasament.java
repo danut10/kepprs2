6ddb4c91b941ffa26cac929c532df9b55181f6bf
George <intern@192.168.8.212>
2017-07-13 16:15:48 +0300

    Signed-off-by: George <intern@192.168.8.212>

--- kepres2Web/src/main/java/ro/kepler/kepres/web/controller/ControllerAtasament.java
+++ kepres2Web/src/main/java/ro/kepler/kepres/web/controller/ControllerAtasament.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ro.kepler.kepres.app.dao.DaoAtasament;
 import ro.kepler.kepres.app.dao.DaoAtasament1;
 import ro.kepler.kepres.common.dataRecords.Atasament;
-import ro.kepler.kepres.common.valueObjects.AppSettings;
 
 @Controller
 @RequestMapping("/atasament")
