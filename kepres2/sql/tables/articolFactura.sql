627c37371ea514d1d269c060bb200faeb14390c2
Dan Maxim <dmaxim@kepler.ro>
2017-07-17 16:32:49 +0300

    Signed-off-by: Dan Maxim <dmaxim@kepler.ro>

--- kepres2/sql/tables/articolFactura.sql
+++ kepres2/sql/tables/articolFactura.sql
@@ -5,7 +5,7 @@ create table ARTICOL_FACTURA
 	  FACTURA nvarchar2(10) not null 
 	, NR_CRT number(2,0) not null
 	, UM nvarchar2(15) not null
-	, CANT decimal(10, 2) not null
+	, CANTITATEA decimal(10, 2) not null
 	, PRET_UNITAR decimal(10, 2) not null
 	, SUMA decimal(10, 2) not null
 	, TVA decimal(10, 2) not null
