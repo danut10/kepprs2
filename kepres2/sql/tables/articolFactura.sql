13ab4bcef37cee0fa6a7c625cf742266715aefc7
Dan Maxim <dmaxim@kepler.ro>
2017-07-17 16:30:00 +0300

    Signed-off-by: Dan Maxim <dmaxim@kepler.ro>

--- kepres2/sql/tables/articolFactura.sql
+++ kepres2/sql/tables/articolFactura.sql
@@ -1,17 +1,17 @@
-drop table ARTICOLFACTURA;
+drop table ARTICOL_FACTURA;
 
-create table ARTICOLFACTURA
+create table ARTICOL_FACTURA
 (
-	  Factura nvarchar2(10) not null 
-	, nrCrt number(10) not null
-	, UM nvarchar2(25) not null
-	, Cantitate decimal(10, 2) not null
-	, Pret_Unitar decimal(10, 2) not null
-	, Suma decimal(10, 2) not null
-	, Tva decimal(10, 2) not null
-	, Total decimal(10, 2) not null
-	, Descriere nvarchar2(500) 
+	  FACTURA nvarchar2(10) not null 
+	, NR_CRT number(2,0) not null
+	, UM nvarchar2(15) not null
+	, CANT decimal(10, 2) not null
+	, PRET_UNITAR decimal(10, 2) not null
+	, SUMA decimal(10, 2) not null
+	, TVA decimal(10, 2) not null
+	, TOTAL decimal(10, 2) not null
+	, DESCRIERE nvarchar2(100) 
 );
 
-drop sequence SEQ_ARTICOLFACTURA;
-create sequence SEQ_ARTICOLFACTURA;
+drop sequence SEQ_ARTICOL_FACTURA;
+create sequence SEQ_ARTICOL_FACTURA;
